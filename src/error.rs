c62d9180a8d961e50bbd1e5fddee612bda2ebae0
AndreVuillemot160 <84594011+AndreVuillemot160@users.noreply.github.com>
2021-07-02 18:26:28 +0000

    impl Error (#8)
    @bltzxsd take a look
    resolves https://github.com/AndreVuillemot160/QuickDash/issues/3

--- src/error.rs
+++ src/error.rs
@@ -14,41 +14,46 @@
  */
 
 /// Enum representing each way the appication can fail.
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-pub enum Error {
-    /// No errors occured, everything executed correctly.
-    NoError,
+use std::fmt::{self, Formatter};
+
+/// Enum representing each way the appication can fail.
+#[derive(Debug)]
+pub enum Error<'err> {
     /// Parsing of command-line options failed.
-    OptionParsingError,
+    ArgumentParsingError(clap::Error),
     /// Selected and saved hash lengths differ.
-    HashLengthDiffers,
+    HashLengthDiffers(i32, i32),
     /// Parsing the hashes file failed.
-    HashesFileParsingFailure,
+    HashesFileParsingFailure(&'err str),
     /// The specified amount of files do not match.
     NFilesDiffer(i32),
 }
 
-impl Error {
-    /// Get the executable exit value from an `Error` instance.
-    pub fn exit_value(&self) -> i32 {
+
+impl<'err> std::error::Error for Error<'_> {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match *self {
-            Error::NoError => 0,
-            Error::OptionParsingError => 1,
-            Error::HashLengthDiffers => 2,
-            Error::HashesFileParsingFailure => 3,
-            Error::NFilesDiffer(i) => i + 3,
+            // the source here is clap so we can call that
+            Error::ArgumentParsingError(ref clap_err) => Some(clap_err),
+            // couldn't think of anything to use as source here since we only use the numbers to show the length
+            Error::HashLengthDiffers(_, _) => None,
+            // I am not confident in this, parsing failure can happen for a variety of reasons. 
+            // Possible enum for this?  
+            Error::HashesFileParsingFailure(ref err) => Some(err),
+            // No source here either,  just gives the number of files that differ. 
+            Error::NFilesDiffer(_) => None,
         }
     }
 }
 
-impl From<i32> for Error {
-    fn from(i: i32) -> Self {
-        match i {
-            0 => Error::NoError,
-            1 => Error::OptionParsingError,
-            2 => Error::HashLengthDiffers,
-            3 => Error::HashesFileParsingFailure,
-            i => Error::NFilesDiffer(i - 3),
+
+impl<'err> fmt::Display for Error<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            Error::ArgumentParsingError(err) => write!(f, "CL Argument Parsing Error: {}", err),
+            Error::HashLengthDiffers(h1_len, h2_len) => write!(f, "Hash lengths Differ! Hash 1: {} Hash 2: {}", h1_len, h2_len),
+            Error::HashesFileParsingFailure(err) => write!(f, "Error while Parsing Files: {}", err),
+            Error::NFilesDiffer(num) => write!(f, "Files Differ: {}", num),
         }
     }
 }
